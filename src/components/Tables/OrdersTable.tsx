bc25d4b7af5a3d9291c88a1437d8770bfd1468cf
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-02-08 03:03:36 +0500

    Updated

--- src/components/Tables/OrdersTable.tsx
+++ src/components/Tables/OrdersTable.tsx
@@ -22,7 +22,7 @@ const OrderTable: React.FC<OrderTableProps> = ({ orders, onUpdateStatus }) => {
   };
 
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1 min-h-screen">
+    <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
