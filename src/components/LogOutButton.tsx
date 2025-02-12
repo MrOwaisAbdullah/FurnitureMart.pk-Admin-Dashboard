47f5f9967e11a252f497805ce9b3a55a280fae21
Owais Abdullah <mrowaisabdullah@gmail.com>
2025-02-12 15:15:32 +0500

    Updated

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -30,7 +30,7 @@ const LogoutButton = () => {
   }
 
   return (
-    <SignOutButton redirectUrl="/">
+    <SignOutButton>
       <button
         className={`group relative flex w-full items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}
       >
